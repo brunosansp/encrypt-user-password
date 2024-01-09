c8000be6167762b298ca8e6b9d872e6e7d7b10ca
Bruno Santos <brunoadsjp@gmail.com>
2024-01-08 22:12:04 -0300

    implements encrypt password with BCrypt

--- src/main/java/br/com/brunosansp/encryptuserpassword/Application.java
+++ src/main/java/br/com/brunosansp/encryptuserpassword/Application.java
@@ -2,12 +2,24 @@ package br.com.brunosansp.encryptuserpassword;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+/***
+ * ao adicionar (exclude = { SecurityAutoConfiguration.class }) a aplicação não irá
+ * mais validar usuário e senha
+ */
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/br/com/brunosansp/encryptuserpassword/controllers/UserController.java
+++ src/main/java/br/com/brunosansp/encryptuserpassword/controllers/UserController.java
@@ -4,22 +4,27 @@ import br.com.brunosansp.encryptuserpassword.models.UserModel;
 import br.com.brunosansp.encryptuserpassword.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuario")
 public class UserController {
     
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
     
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, PasswordEncoder encoder) {
         this.userRepository = userRepository;
+        this.encoder = encoder;
     }
     
     @GetMapping("/listar-todos")
@@ -29,6 +34,21 @@ public class UserController {
     
     @PostMapping("/salvar")
     public ResponseEntity<UserModel> save(@RequestBody UserModel user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
+    }
+    
+    @GetMapping("/validar-senha")
+    public ResponseEntity<Boolean> passwordValidate(@RequestParam String login,
+                                                    @RequestParam String password) {
+        Optional<UserModel> optUser = userRepository.findByLogin(login);
+        if (optUser.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+        
+        UserModel user = optUser.get();
+        boolean valid = encoder.matches(password, user.getPassword());
+        HttpStatus status = valid ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+        return ResponseEntity.status(status).body(valid);
     }
 }
